cceb80fafafbce0da62e4b43f5158604de47e5bc
Jake Bogan <jakebogan01@gmail.com>
2023-04-05 13:42:40 -0400

    added player animations for idle and walking

--- Kitchen Chaos/Assets/Scripts/Player.cs
+++ Kitchen Chaos/Assets/Scripts/Player.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 7f;
-    private void Update()
-    {
+    [SerializeField] private float playerMovementSpeed = 7f;
+
+    private bool isPlayerWalking;
+
+    private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
-        if (Input.GetKey(KeyCode.W))
-        {
+        if (Input.GetKey(KeyCode.W)) {
             inputVector.y += 1;
         }
-        if (Input.GetKey(KeyCode.S))
-        {
+        if (Input.GetKey(KeyCode.S)) {
             inputVector.y -= 1;
         }
-        if (Input.GetKey(KeyCode.A))
-        {
+        if (Input.GetKey(KeyCode.A)) {
             inputVector.x -= 1;
         }
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.D)) {
             inputVector.x += 1;
         }
 
@@ -32,14 +30,28 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * movementSpeed * Time.deltaTime;
+        transform.position += moveDir * playerMovementSpeed * Time.deltaTime;
+
+        /*****
+         * ternary operator setting isPlayerWalking true or false
+         * comparing if moveDir vector is not (0,0,0)
+        *****/
+        isPlayerWalking = moveDir != Vector3.zero;
 
         /*****
          * allows characters to rotate direction smoothly
          * Slerp spherical interpolations
          * Slerp(a, b, t) t returns the difference between point a (starting point vector) and b (target point vector)
         *****/
-        float characterRotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * characterRotateSpeed);
+        float playerRotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * playerRotateSpeed);
+    }
+
+    /*****
+        * public function for PlayerAnimator script
+        * returns true or false for isPlayerWalking
+    *****/
+    public bool IsWalking() {
+        return isPlayerWalking;
     }
 }
---
+++ Kitchen Chaos/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    /*****
+        * letting this script aware of player gameobject
+    *****/
+    [SerializeField] private Player player;
+
+    private Animator animtor;
+
+    /*****
+        * value based of animator parameter IsWalking
+    *****/
+    private const string IS_PLAYER_WALKING = "IsWalking";
+
+    private void Awake() {
+        animtor = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        /*****
+            * changing the animator parameter value based on players vector movement in Player script
+        *****/
+        animtor.SetBool(IS_PLAYER_WALKING, player.IsWalking());
+    }
+}
